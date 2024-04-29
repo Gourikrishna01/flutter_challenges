1baefc744605b229cd7ec31b937b3b2518863ea9
Gourikrishna01 <gourimadathil2001@gmail.com>
2024-04-29 20:24:32 +0530

    message

--- basic_app/lib/main.dart
+++ basic_app/lib/main.dart
@@ -17,12 +17,23 @@ class MyApp extends StatelessWidget {
           title: Text(
             'Flutter',
             style: TextStyle(
-              fontSize: 50,
+              fontSize: 20,
             ),
           ),
           centerTitle: true,
           backgroundColor: Colors.blue,
+          //leading //
           leading: Icon(Icons.home_outlined),
+          //actions//
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz_outlined))
+          ],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+          ),
+          //elevation//
+          elevation: 10.5,
         ),
         body: Center(
           child: Text(
