d553f63bfd2b217181682e63390b767862dfc62b
Gourikrishna01 <gourimadathil2001@gmail.com>
2024-05-06 20:27:00 +0530

    message

---
+++ textfield_widget/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "text_widget",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Text_widget_field",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+          leading: Icon(Icons.person_2),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  print("welcome");
+                },
+                icon: Icon(Icons.search)),
+            IconButton(
+                onPressed: () {
+                  print("accounts");
+                },
+                icon: Icon(Icons.more_horiz))
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Center(
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "username",
+                      hintText: "enter your username",
+                      prefixIcon: Icon(Icons.verified_user),
+                      suffixIcon: Icon(Icons.abc)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "username",
+                      hintText: "enter your username",
+                      suffixText: "mr",
+                      helperText: "enter the username or email",
+                      hintStyle: TextStyle(
+                        color: Colors.black26,
+                        fontSize: 25,
+                      ),
+                      labelStyle: TextStyle(color: Colors.red)),
+                ),
+                TextField(
+                  maxLength: 10,
+                  obscureText: false,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
