ca26d2f3fb4f79d9649498e2052be93da31276d4
Gourikrishna01 <gourimadathil2001@gmail.com>
2024-05-25 12:19:34 +0530

    message

--- radiowidget/lib/main.dart
+++ radiowidget/lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String? gender;
+  String? status;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,6 +30,84 @@ class _MyAppState extends State<MyApp> {
           actions: [
             IconButton(onPressed: () {}, icon: Icon(Icons.search)),
             IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz))
+          ],
+        ),
+        body: Column(
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "select your gender",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    ListTile(
+                      title: Text("Male"),
+                      leading: Radio(
+                          value: "male",
+                          groupValue: gender,
+                          onChanged: (String? value) {
+                            setState(() {
+                              gender = value;
+                            });
+                          }),
+                    ),
+                    ListTile(
+                      title: Text("female"),
+                      leading: Radio(
+                          value: "female",
+                          groupValue: gender,
+                          onChanged: (String? value) {
+                            setState(() {
+                              gender = value;
+                            });
+                          }),
+                    ),
+                    ListTile(
+                      title: Text("other"),
+                      leading: Radio(
+                          value: "other",
+                          groupValue: gender,
+                          onChanged: (String? value) {
+                            setState(() {
+                              gender = value;
+                            });
+                          }),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+                child: Column(
+              children: [
+                Text(
+                  "Material Status",
+                  style: TextStyle(fontSize: 20),
+                ),
+                RadioListTile(
+                    title: Text("married"),
+                    value: "married",
+                    groupValue: status,
+                    onChanged: (String? val) {
+                      setState(() {
+                        status = val;
+                      });
+                    }),
+                RadioListTile(
+                    activeColor: Colors.red,
+                    title: Text("unmarried"),
+                    value: "unmarried",
+                    groupValue: status,
+                    onChanged: (String? val) {
+                      setState(() {
+                        status = val;
+                      });
+                    })
+              ],
+            ))
           ],
         ),
       ),
